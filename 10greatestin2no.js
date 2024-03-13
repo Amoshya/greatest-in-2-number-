51f862d55f97b041ba5c622d2cb8ec1f76183800
Amoshya <161802948+Amoshya@users.noreply.github.com>
2024-03-13 12:42:34 +0530

    Add files via upload

---
+++ 10greatestin2no.js
@@ -0,0 +1,9 @@
+const input  = require("readline-sync");
+
+let a = input.questionInt("ENTER A NO A :- ");
+let b = input.questionInt("ENTER A NO B ;- ");
+if (a>b)
+{
+    console.log("A IS GREATER ");
+}else {
+console.log("B IS GREATER ");}
